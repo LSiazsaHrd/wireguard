797a7b159de0ccf338fdbaa2ae47a51c379f3fef
LSiazsaHrd <lsiazsahrd@gmail.com>
2021-03-27 23:05:08 +0300

    Update wg-ubuntu-server-up.sh

--- wg-ubuntu-server-up.sh
+++ wg-ubuntu-server-up.sh
@@ -83,8 +83,8 @@ echo ---------------------------------------------------configure firewall rules
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 55000 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A INPUT -s 10.0.0.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -s 10.0.1.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -s 10.0.1.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 
 # make firewall changes persistent
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
@@ -120,9 +120,9 @@ server:
     # IPs authorised to access the DNS Server
     access-control: 0.0.0.0/0                 refuse
     access-control: 127.0.0.1                 allow
-    access-control: 10.0.0.0/24             allow
+    access-control: 10.0.1.0/24             allow
     # not allowed to be returned for public Internet  names
-    private-address: 10.0.0.0/24
+    private-address: 10.0.1.0/24
     #hide DNS Server info
     hide-identity: yes
     hide-version: yes
@@ -149,7 +149,7 @@ server:
     # ensure kernel buffer is large enough to not lose messages in traffic spikes
     so-rcvbuf: 1m
     # ensure privacy of local IP ranges
-    private-address: 10.0.0.0/24
+    private-address: 10.0.1.0/24
 ENDOFFILE
 
   # give root ownership of the Unbound config
